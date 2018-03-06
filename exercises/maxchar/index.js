60e8942fc88e0cdda8df05f7e55ab795a4c89e36
Maciej Kurek <maciejk77@gmail.com>
2018-03-06 15:51:39 +0000

    charMap pattern, creating object map and finding max values

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,23 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  let charMap = {};
+  let maxValue = 0;
+  let maxChar = '';
+
+  for(let letter of str) {
+    !charMap[letter] ? charMap[letter] = 1 : charMap[letter]++;
+  }
+
+  for(let char in charMap) {
+    if(charMap[char] > maxValue) {
+      maxValue = charMap[char];
+      maxChar = char;
+    }
+  }
+  
+  return maxChar;
+}
 
 module.exports = maxChar;
