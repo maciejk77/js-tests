81f1571b1789f6ac67007f0b8438e9b8724f4d7a
Maciej Kurek <maciejk77@gmail.com>
2018-03-06 13:11:46 +0000

    Reverse string with reduce

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,12 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  // return str.split('').reduce((reversed, character) => {
+  //   return character + reversed; 
+  // }, '');
+
+  return str.split('').reduce((rev, char) => char + rev, '');
+}
 
 module.exports = reverse;
