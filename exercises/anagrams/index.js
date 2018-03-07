aa14bc13d001db4f19c01c9d1a0ca01977a1a1bf
Maciej Kurek <maciejk77@gmail.com>
2018-03-07 17:44:58 +0000

    Anagram solution2 easier

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -9,29 +9,43 @@
 //   anagrams('Hi there', 'Bye there') --> False
 
 function anagrams(stringA, stringB) {
-  const aCharMap = buildCharMap(stringA);
-  const bCharMap = buildCharMap(stringB);
 
-  if(Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
-    return false;
-  }
-
-  for(let char in aCharMap) {
-    if(aCharMap[char] !== bCharMap[char]) {
-      return false;
-    }
-  }
-
-  function buildCharMap(str) {
-    const charMap = {};
-    
-    for(let char of str.replace(/[^\w]/g, '').toLowerCase()) {
-      charMap[char] = charMap[char] + 1 || 1;
-    }
-    return charMap; 
-  }
+  return cleanString(stringA) === cleanString(stringB);
   
-  return true;    
+  function cleanString(str) {
+    return str.replace(/[^w]/g, '').toLowerCase().split('').sort().join('');
+  }
 }
+ 
+// SOLUTION 1
+//   const aCharMap = buildCharMap(stringA);
+//   const bCharMap = buildCharMap(stringB);
+
+//   if(Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+//     return false;
+//   }
+
+//   for(let char in aCharMap) {
+//     if(aCharMap[char] !== bCharMap[char]) {
+//       return false;
+//     }
+//   }
+
+//   function buildCharMap(str) {
+//     const charMap = {};
+    
+//     for(let char of str.replace(/[^\w]/g, '').toLowerCase()) {
+//       charMap[char] = charMap[char] + 1 || 1;
+//     }
+//     return charMap; 
+//   }
+  
+//   return true;    
+// }
+
+// SOLUTION 2
+
+
+
 
 module.exports = anagrams;
